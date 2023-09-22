94db50ea5717631996bb816400ea505892bd96df
frankbungey <frankbungey@gmail.com>
2023-09-22 18:58:20 +0300

    my first project

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('#menuBtn');
+const sideNav = document.querySelector('#sideNav');
+
+sideNav.style.right = "-250px";
+menuBtn.onclick=()=>{ 
+if(sideNav.style.right == "-250px"){
+    sideNav.style.right="0"
+}
+else{
+    sideNav.style.right= "-250px"
+}
+}
